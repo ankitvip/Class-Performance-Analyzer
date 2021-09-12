b64d4abe9541994375b194aebe6aae6ccd9117fa
ankit <as71704@gmail.com>
2021-09-12 18:25:26 +0530

    class performance analyzer

---
+++ cpa2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\noman\desktop\class-performance-analyzer\cpa2\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
